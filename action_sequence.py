67c68e03ee5da5fde7b34cc5bd06d2e8095bc961
heathcliffYang <ginny0922fc2@gmail.com>
2019-05-13 10:15:25 +0800

    confusion matrix and one round only training

--- action_sequence.py
+++ action_sequence.py
@@ -9,7 +9,7 @@ import PIL.Image
 from rnn import *
 from basic_tool import *
 from sklearn.neighbors import NearestNeighbors
-
+import confusion_matrix_tool as cmt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -33,36 +33,48 @@ latitude_list = np.array([max(wifi_loc_time[:, 521]), -1\
 ## KNN initial calculation
 nbrs = NearestNeighbors(n_neighbors=3, algorithm='ball_tree').fit(wifi_loc_time[:,:520])
 distances, indices = nbrs.kneighbors(wifi_loc_time[:,:520])
+distances_test, indices_test = nbrs.kneighbors(wifi_loc_time_test[:,:520])
 
 # Training setting
-n_episodes = 300
+n_episodes = 1000
 total_loss = np.zeros((n_episodes,))
 radius_gt = 0.5
 delta = 0.5
 avg_distance = np.zeros((n_episodes,2))
 avg_iow = np.zeros((n_episodes,2))
 avg_grading = np.zeros((n_episodes,5))
+avg_conti_score = np.zeros((n_episodes,))
+batch_size = 1
+action_history_batch = torch.zeros((batch_size, 5, 5))
+action_label_batch = np.zeros((batch_size,5))
+
+test_distance = np.zeros((2,))
+test_iow = np.zeros((2,))
+test_grading = np.zeros((5,))
+
 
 # Model setting
 input_size = 3
-hidden_size = 30
+hidden_size = 256
 output_size = 5
 wifi_size = 520
-lr = 0.001
-lambda1 = lambda e: 0.9**(e/2)
+lr = 0.1
+lambda1 = lambda e: 0.85**(e/2)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 rl_rnn = RL_RNN(input_size, hidden_size, output_size, wifi_size, lr, device, lambda1)
 
 # Training process
 for e in range(n_episodes):
     rl_rnn.net.train()
-    if e % 100 == 0:
+    if e % 100 == 99:
         rl_rnn.peek_weights()
     rl_rnn.lr_sche.step()
 
+    round_idx = int(e/100) % 5
+    # round_idx = 4
+    y_true = np.zeros((len(wifi_loc_time),5))
+    y_pred = np.zeros((len(wifi_loc_time),5))
     for i in range(len(wifi_loc_time)):
-        Goal = False
-
         ## 1. KNN locates initial coordinates and radius
         Hx = 0.
         Hy = 0.
@@ -79,13 +91,8 @@ for e in range(n_episodes):
             IoW_cur = IoW(wifi_loc_time[i, 520:522], coordinate, radius_gt, radius)
             if (IoW_cur == 0):
                 radius *= 1.5
-            elif (IoW_cur > delta):
-                Goal = True
-                break
             else:
                 break
-        if (Goal == True):
-            continue
 
         right_action_set = []
         right_coor = coordinate.copy()
@@ -96,26 +103,99 @@ for e in range(n_episodes):
             right_action_set.append(right)
 
         input = np.concatenate((coordinate, np.array([radius])))
-        action_history, coordinate, radius = rl_rnn.forward_5_rounds(wifi_loc_time[i,:520], coordinate, radius)
+        # ver. 1
+        action_history, coordinate, radius = rl_rnn.forward_5_rounds(wifi_loc_time[i,:520], coordinate, radius, 1)
+        # ver. 2
+        # action_history, coordinate, radius = rl_rnn.forward_down(wifi_loc_time[i,:520], coordinate, radius)
 
         avg_distance[e,0] += dis(wifi_loc_time[i, 520:522], coordinate)
         avg_iow[e,0] += IoW(wifi_loc_time[i, 520:522], coordinate, radius_gt, radius)
         avg_distance[e,1] += dis(wifi_loc_time[i, 520:522], right_coor)
         avg_iow[e,1] += IoW(wifi_loc_time[i, 520:522], right_coor, radius_gt, right_radius)
-        avg_grading[e,] += rl_rnn.grading(action_history, right_action_set)
+        grading_sheet, ans_sheet = rl_rnn.grading(action_history, right_action_set)
+        avg_grading[e,] += grading_sheet
+        avg_conti_score[e] += rl_rnn.conti_success_score(grading_sheet)
 
-        loss = rl_rnn.back_propagation(action_history, np.array(right_action_set))
-        total_loss[e] += loss
+
+        if batch_size != 1:
+            print(right_action_set)
+            batch_iter = i % batch_size
+            if batch_iter != batch_size -1:
+                action_history_batch[batch_iter] = action_history
+                action_label_batch[batch_iter] = np.array(right_action_set)
+            else:
+                loss = rl_rnn.back_propagation(action_history_batch, action_label_batch, 1)
+                total_loss[e] += loss
+        else:
+            loss = rl_rnn.back_propagation(action_history, np.array(right_action_set), 1)
+            total_loss[e] += loss
+
+        # confusion_matrix_stack
+        y_true[i] = np.array(right_action_set)
+        y_pred[i] = ans_sheet
 
     avg_distance[e,:] /= len(wifi_loc_time)
-    avg_grading[e, :] /= len(wifi_loc_time)
+    avg_grading[e, :] #/= len(wifi_loc_time)
     avg_iow[e,:] /= len(wifi_loc_time)
-    total_loss[e] /= len(wifi_loc_time)
-    print("Epoch - ", e, ", avg loss :", total_loss[e], ", lr is :%.6f"%(lambda1(e)), ", avg distance:", avg_distance[e], ", avg IoW:", avg_iow[e])
-    print("                                                                                         Accuracy of 5 rounds:", avg_grading[e])
+    avg_conti_score[e] /= len(wifi_loc_time)
+    print("Epoch -", e, round_idx,"avg loss :", total_loss[e], ", lr is :%.6f"%(lambda1(e)), ", avg distance:", avg_distance[e], "\navg IoW:", avg_iow[e], ", avg cont_score:", avg_conti_score[e])
+    print("Accuracy of 5 rounds:", avg_grading[e])
+    for l in range(5):
+        cmt.plot_confusion_matrix(y_true[:,l], y_pred[:,l], classes=['R'+str(i) for i in range(5)], normalize=False, title='Normalized confusion matrix')
+        plt.savefig("cm/Conf_matrix_e_{}_round_{}.png".format(e, l))
+        plt.close()
+        plt.cla()
+        plt.clf()
+
+    #Testing
+    rl_rnn.net.eval()
+    test_distance = np.zeros((2,))
+    test_iow = np.zeros((2,))
+    test_grading = np.zeros((5,))
+    test_conti_score = 0
+    for i in range(len(wifi_loc_time_test)):
+        Hx = 0.
+        Hy = 0.
+        for m in range(3):
+            Hx += wifi_loc_time[indices_test[i, m], 520]
+            Hy += wifi_loc_time[indices_test[i, m], 521]
+        Hx /= 3.
+        Hy /= 3.
+        coordinate = np.array([Hx, Hy])
+        radius = 1.
+
+        while True:
+            IoW_cur = IoW(wifi_loc_time_test[i, 520:522], coordinate, radius_gt, radius)
+            if (IoW_cur == 0):
+                radius *= 1.5
+            else:
+                break
+
+        right_action_set = []
+        right_coor = coordinate.copy()
+        right_radius = radius
+        for t in range(5):
+            right = right_action(wifi_loc_time_test[i, 520:522], right_coor, radius_gt, right_radius)
+            right_coor, right_radius = take_action(right, right_coor, right_radius)
+            right_action_set.append(right)
+
+        input = np.concatenate((coordinate, np.array([radius])))
+        action_history, coordinate, radius = rl_rnn.forward_down(wifi_loc_time_test[i,:520], coordinate, radius)
+
+        test_distance[0] += dis(wifi_loc_time_test[i, 520:522], coordinate)
+        test_iow[0] += IoW(wifi_loc_time_test[i, 520:522], coordinate, radius_gt, radius)
+        test_distance[1] += dis(wifi_loc_time_test[i, 520:522], right_coor)
+        test_iow[1] += IoW(wifi_loc_time_test[i, 520:522], right_coor, radius_gt, right_radius)
+        grading_sheet, _ = rl_rnn.grading(action_history, right_action_set)
+        test_grading += grading_sheet
+        test_conti_score += rl_rnn.conti_success_score(grading_sheet)
+
+    print("Test distance: {}, iow: {}\ngrading:{}, conti_score:{}\n".format(test_distance/len(wifi_loc_time_test), test_iow/len(wifi_loc_time_test), test_grading/len(wifi_loc_time_test), test_conti_score/len(wifi_loc_time_test)))
+    
 
 
-np.savetxt("avg_distance.csv", avg_distance, delimiter=',')
-np.savetxt("avg_iow.csv", avg_iow, delimiter=',')
-np.savetxt("avg_grading.csv", avg_grading, delimiter=',')
-np.savetxt("total_loss.csv", total_loss, delimiter=',')
+
+# np.savetxt("plot/avg_distance.csv", avg_distance, delimiter=',')
+# np.savetxt("plot/avg_iow.csv", avg_iow, delimiter=',')
+# np.savetxt("plot/avg_grading.csv", avg_grading, delimiter=',')
+# np.savetxt("plot/total_loss.csv", total_loss, delimiter=',')
--- rnn.py
+++ rnn.py
@@ -2,53 +2,92 @@ import numpy as np
 import torch.nn as nn
 import torch
 from basic_tool import *
+import math
+
+class_weight_set = torch.tensor([[0.1, 2*math.sqrt(2), 2, 2, math.sqrt(2)],
+                                 [2*math.sqrt(2), 0.1, 2, 2, math.sqrt(2)],
+                                 [2, 2, 0.1, 2*math.sqrt(2), math.sqrt(2)],
+                                 [2, 2, 2*math.sqrt(2), 0.1, math.sqrt(2)],
+                                 [math.sqrt(2), math.sqrt(2), math.sqrt(2), math.sqrt(2), 0.1]])
 
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, wifi_size, device):
+    def __init__(self, input_size, hidden_size, output_size, wifi_size, device, mode='RNN'):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.device = device
+        self.mode = mode
 
-        self.wifi_w = nn.Linear(wifi_size, hidden_size)
-        self.i2m = nn.Linear(input_size + hidden_size, input_size + hidden_size)
-        self.m_relu = nn.ReLU()
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        if mode == 'RNN':
+            print("RNN model")
+            self.wifi_w = nn.Linear(wifi_size, hidden_size)
+            self.i2m = nn.Linear(input_size + hidden_size + 25, input_size + hidden_size)
+            # self.i2m = nn.Linear(input_size + hidden_size, input_size + hidden_size)
+            self.m_relu = nn.ReLU()
+            self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+            self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        else:
+            self.wifi_w = nn.Linear(wifi_size, hidden_size)
+            self.gru = nn.GRU(input_size, hidden_size)
+            self.i2o = nn.Linear(hidden_size, output_size)
 
-    def forward(self, input, hidden): 
-        combined = torch.cat((input, hidden), 0).to(self.device)
-        combined = self.i2m(combined)
-        combined = self.m_relu(combined)
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
+    def forward(self, input, hidden, action_history):
+        if self.mode == 'RNN':
+            action_history = action_history.view(-1,).to(self.device)
+            combined = torch.cat((input, hidden, action_history), 0).to(self.device)
+            # combined = torch.cat((input, hidden), 0).to(self.device)
+            combined = self.i2m(combined)
+            combined = self.m_relu(combined)
+            hidden = self.i2h(combined)
+            output = self.i2o(combined)
+        else:
+            output, hidden = self.gru(input.view(1,1,-1), hidden.view(1,1,-1))
+            output = self.i2o(output)
         return output, hidden
+
 
     def h0_init(self, wifi):
         input_wifi = torch.FloatTensor(-wifi).to(self.device)
         h0 = self.wifi_w(input_wifi)
         return h0
 
+
 class RL_RNN(object):
     def __init__(self, input_size, hidden_size, output_size, wifi_size, lr, device, lambda1):
-        self.net = RNN(input_size, hidden_size, output_size, wifi_size, device).to(device)
+        self.net = RNN(input_size, hidden_size, output_size, wifi_size, device, mode='RNN').to(device)
         self.optimizer = torch.optim.Adam(self.net.parameters(), lr=lr)
-        self.Loss = nn.CrossEntropyLoss()
+        self.Loss = []
+        for i in range(5):
+            print('weight', class_weight_set[i])
+            self.Loss.append(nn.CrossEntropyLoss(weight=class_weight_set[i]))
         self.device = device
         self.lr_sche = torch.optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=lambda1, last_epoch=-1)
 
-    def forward_5_rounds(self, wifi, coordinate, radius):
+    def forward_5_rounds(self, wifi, coordinate, radius, round):
         h0 = self.net.h0_init(wifi)
         action_history = torch.zeros([5,5])
-        for i in range(5):
+        for i in range(round):
+            # print("forward",i)
             input = torch.FloatTensor(np.concatenate((coordinate, np.array([radius])))).to(self.device)
-            action, h0 = self.net.forward(input, h0)
+            action, h0 = self.net.forward(input, h0, action_history)
             action_history[i] = action
             coordinate, radius = take_action(torch.argmax(action).item(), coordinate, radius)
         return action_history, coordinate, radius
 
-    def back_propagation(self, action_history, action_label):
-        # print("action_history", action_history)
-        loss = self.Loss(action_history, torch.LongTensor(action_label))
+    def forward_down(self, wifi, coordinate, radius):
+        h0 = self.net.h0_init(wifi)
+        action_history = torch.zeros([5,5])
+        for i in range(5):
+            input = torch.FloatTensor(np.concatenate((coordinate, np.array([radius])))).to(self.device)
+            action, h0 = self.net.forward(input, h0, action_history)
+            action_history[i] = action
+            coordinate, radius = take_action(torch.argmax(action).item(), coordinate, radius)
+        return action_history, coordinate, radius
+
+    def back_propagation(self, action_history, action_label, round):
+        loss = 0
+        for i in range(round):
+            # print("backward {}".format(i))
+            loss += self.Loss[action_label[i]](action_history[i].view(1,-1), torch.LongTensor([action_label[i]]))
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -61,7 +100,20 @@ class RL_RNN(object):
 
     def grading(self, action_history, action_label):
         grading_sheet = np.zeros((5,))
+        ans_sheet = np.zeros((5,))
         for i in range(5):
+            ans_sheet[i] = torch.argmax(action_history[i]).item()
             if (torch.argmax(action_history[i]).item() == action_label[i]):
                 grading_sheet[i] = 1
-        return grading_sheet
+        
+        return grading_sheet, ans_sheet
+
+    def conti_success_score(self, grading_sheet):
+        score = 0
+        for i in range(5):
+            if grading_sheet[i] == 1:
+                score += 1
+            else:
+                break
+
+        return score
